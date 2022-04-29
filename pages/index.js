00c2eb1c2478dcc4b213f76ecb82445d9b0012de
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2022-04-29 23:50:22 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Content from '../components/Content'
 export default function Home() {
   return (
@@ -14,10 +15,19 @@ export default function Home() {
         <meta name="twitter:title" content="Mohamed Sadiq" />
         <meta name="twitter:description" content="A product designer" />
         <meta name="twitter:image" content="https://i.ibb.co/Cvc4f6R/Instagram-post-6.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H699TZ29QW"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-H699TZ29QW');
+        </script>
       </Head>
       <main>
        <Content />
       </main>
+      
       </div>
   )
 }
