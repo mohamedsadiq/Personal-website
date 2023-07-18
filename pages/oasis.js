8a70ef0a264a485adb4d01f256b0567f19e1518d
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2023-07-18 16:13:26 +0200

    update the links

--- pages/oasis.js
+++ pages/oasis.js
@@ -332,7 +332,7 @@ const Taste = () => {
                     <div className="flex-container flex-container-level2">
                         <div className="item3">
                             <h2>Podcast</h2>
-                            <p>Books I loved reading.</p>
+                            <p>Hear the shows that have me hooked!.</p>
                             <div className="podcasts_taste">
                                 <div className="thenetworkstate hover_img_podcast">
                                     <Image
@@ -390,7 +390,7 @@ const Taste = () => {
                         </div>
                         <div className="item4">
                             <h2>Twitter</h2>
-                            <p>Books I loved reading.</p>
+                            <p>Join the Discourse: Tap into Twitter's Realm!.</p>
                             <div className="twitterIcons">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="90" height="120"><path fill="#03A9F4" d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429" /></svg>
 
@@ -411,7 +411,7 @@ const Taste = () => {
                     <div className="flex-container flex-container-level2">
                         <div className="item3">
                             <h2>Dribbble</h2>
-                            <p>Follow me on Dribbble</p>
+                            <p>Follow me on Dribbble.</p>
                             <div className="podcasts_taste dribbble">
                                 <svg width="100" height="70" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="100" height="100" fill="#000" fillOpacity="0" /><circle cx="50" cy="50" r="48" fill="#EA4C89" /><path fillRule="evenodd" clipRule="evenodd" d="M50 0C22.397 0 0 22.397 0 50C0 77.603 22.397 100 50 100C77.5488 100 100 77.603 100 50C100 22.397 77.5488 0 50 0ZM83.026 23.0477C88.9913 30.3145 92.5705 39.5879 92.679 49.6204C91.269 49.3492 77.1692 46.4751 62.961 48.2646C62.6356 47.5597 62.3644 46.8004 62.0391 46.0412C61.1714 43.9805 60.1952 41.8655 59.2191 39.859C74.9458 33.4599 82.1041 24.2408 83.026 23.0477ZM50 7.37527C60.846 7.37527 70.7701 11.4425 78.308 18.1128C77.5488 19.1974 71.0954 27.82 55.9111 33.5141C48.9154 20.6616 41.1605 10.141 39.9675 8.5141C43.167 7.75488 46.5293 7.37527 50 7.37527ZM31.833 11.3883C32.9718 12.9067 40.564 23.4816 47.6681 36.0629C27.7115 41.3774 10.0868 41.269 8.18872 41.269C10.9544 28.0369 19.9024 17.0282 31.833 11.3883ZM7.26681 50.0542C7.26681 49.6204 7.26681 49.1866 7.26681 48.7527C9.11063 48.8069 29.8265 49.0781 51.1388 42.679C52.3861 45.0651 53.5249 47.5054 54.6095 49.9458C54.0672 50.1085 53.4707 50.2712 52.9284 50.4338C30.9111 57.538 19.1974 76.9523 18.2213 78.5792C11.4425 71.0412 7.26681 61.0087 7.26681 50.0542ZM50 92.7332C40.1302 92.7332 31.0195 89.3709 23.8069 83.731C24.5662 82.1584 33.243 65.4555 57.321 57.0499C57.4295 56.9957 57.4837 56.9957 57.5922 56.9414C63.6117 72.5054 66.0521 85.5748 66.7028 89.3167C61.551 91.5401 55.9111 92.7332 50 92.7332ZM73.807 85.4122C73.3731 82.8091 71.0955 70.3362 65.5098 54.9892C78.9046 52.8742 90.6182 56.3449 92.0824 56.833C90.2386 68.7093 83.4056 78.9588 73.807 85.4122Z" fill="#C32361" /></svg>
                             </div>
@@ -436,7 +436,7 @@ const Taste = () => {
                         </div>
                         <div className="item4">
                             <h2>Substack</h2>
-                            <p>Books I loved reading.</p>
+                            <p>Stay informed and inspired!.</p>
                             <div className="twitterIcons substack">
                                 <svg width="100" xmlns="http://www.w3.org/2000/svg" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 448 511.471"><path fill="#FF681A" d="M0 0h448v62.804H0V0zm0 229.083h448v282.388L223.954 385.808 0 511.471V229.083zm0-114.542h448v62.804H0v-62.804z" /></svg>                          </div>
                             <a href='https://twitter.com/sadiq_moo' target="_blink">
