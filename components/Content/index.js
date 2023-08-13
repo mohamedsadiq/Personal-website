ea232f2f1d70de971ef924a061aa631f563a3655
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2023-08-13 19:53:43 +0200

    update the top of the content

--- components/Content/index.js
+++ components/Content/index.js
@@ -1,53 +1,70 @@
-import { motion } from "framer-motion"
-import Link from "next/link"
-const Content = () => {
-   
-    return  <div className="content">
-         <motion.div
-            className="content_animete"
-            animate={{ opacity: 1, top:73 }}
-            transition={{ 
-            duration: 0.5,
-            delay:1,
-            ease:"easeOut"
-            }}
-          >
-        <div id="name">Mohamed Sadiq</div>
-        <div>
-            <p> A product designer, Founder of <a target="_blank" href="https://www.daosspot.xyz/">DAOs Spot</a> Focusing on <span className="web3">
-            web3
-            <span id="animation_web3"></span>
-            </span>, Open source products <a href="https://twitter.com/developer_dao"  className="links" target="_blank" rel="noreferrer">@DeveloperDAO</a>, member of <a href="https://twitter.com/Anticollective_" className="links" target="_blank" rel="noreferrer">@Anti.</a> you can find me on  <a href="https://x.com/sadiq_moo">X.</a> </p>
-            <Link className="aboutme" href={"/about"}>About me</Link>
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useState, useEffect } from 'react';
+
+const MyComponent = () => {
+    const [divTop, setDivTop] = useState('68vh'); // Default value for smaller heights
+    
+    const updateDivPosition = () => {
+        const screenHeight = window.innerHeight;
+        let newDivTop;
+
+        if (screenHeight > 1200) {
+            newDivTop = '64vh';
+        } else if (screenHeight <= 795) {
+            newDivTop = '73vh';
+        } else {
+            newDivTop = '68vh';
+        }
+
+        setDivTop(newDivTop);
+    };
+
+    useEffect(() => {
+        updateDivPosition();
+        window.addEventListener('resize', updateDivPosition);
+
+        return () => {
+            window.removeEventListener('resize', updateDivPosition);
+        };
+    }, []);
+
+    return (
+        <div className="content" style={{  top:divTop }}>
+            <motion.div
+                className="content_animete"
+                animate={{ opacity: 1, top: 73 }}
+                transition={{
+                    duration: 0.5,
+                    delay: 1,
+                    ease: "easeOut"
+                }}
+            >
+                <div id="name">Mohamed Sadiq</div>
+                <div>
+                    
+
+<p> A product designer, Founder of <a target="_blank" href="https://www.daosspot.xyz/">DAOs Spot</a> Focusing on <span className="web3">
+web3
+<span id="animation_web3"></span>
+</span>, Open source products <a href="https://twitter.com/developer_dao"  className="links" target="_blank" rel="noreferrer">@DeveloperDAO</a>, member of <a href="https://twitter.com/Anticollective_" className="links" target="_blank" rel="noreferrer">@Anti.</a> you can find me on  <a href="https://x.com/sadiq_moo">X.</a> </p>
+<Link className="aboutme" href={"/about"}>About me</Link>
+
+                </div>
+                {/* ... */}
+            </motion.div>
         </div>
-        {/* <div>
-            <h1>Recent activity</h1>
-            <div className="recent_activity">
-                <div className="time">
-                    <span>2023 May / 2</span>
-                    <span>2023 May / 2</span>
-                    <span>2023 May  / 2</span>
-                </div>
-                <div className="time_icon"></div>
-                <div className="project_home">
-                    <div className="">
-                        <h2>DAOs Spot</h2>
-                        <p>A product designer who can code, focusing on web3 Currently building lightweight Web3 experiences at DeveloperDAO and Bulidspace </p>
-                    </div>
-                    <div className="">
-                        <h2>DAOs Spot</h2>
-                        <p>A product designer who can code, focusing on web3 Currently building lightweight Web3 experiences at DeveloperDAO and Bulidspace </p>
-                    </div>
-                    <div className="">
-                        <h2>DAOs Spot</h2>
-                        <p>A product designer who can code, focusing on web3 Currently building lightweight Web3 experiences at DeveloperDAO and Bulidspace </p>
-                    </div>
-                </div>
-            </div>
-        </div> */}
-        </motion.div>
-    </div>
- 
+    );
 }
 
-export default Content
+const Content = () => {
+    return (
+        <div>
+            {/* ... */}
+            <MyComponent />
+        </div>
+    );
+}
+
+export default Content;
+
