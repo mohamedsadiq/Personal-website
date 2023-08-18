99aaaae38f88504106dabf3e62ec468e28b4383f
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2023-08-18 19:55:47 +0200

    update the website

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -6,28 +6,46 @@ const Layout =  ({ children })=> {
     // const children = props.children;
   const [displayChildren, setDisplayChildren] = useState(children);
   const [transitionStage, setTransitionStage] = useState("fadeOut");
-  const contents = "contents"
+  const contents = "contents";
+
   useEffect(() => {
     setTransitionStage("fadeIn");
   }, []);
 
-
-  
   useEffect(() => {
     if (children !== displayChildren) setTransitionStage("fadeOut");
   }, [children, setDisplayChildren, displayChildren]);
-    
+
   const [mode, setMode] = useState(() => {
-    // Retrieve mode value from localStorage, default to "light"
     return typeof window !== 'undefined' && localStorage.getItem("mode") || "light";
   });
-    
+
+  // Browser theme detection logic
   useEffect(() => {
-    // Store mode value in localStorage
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    
+    const handleDarkModeChange = (event) => {
+      if (event.matches) {
+        setMode("dark");
+      } else {
+        setMode("light");
+      }
+    };
+
+    darkModeQuery.addListener(handleDarkModeChange);
+    handleDarkModeChange(darkModeQuery);
+
+    return () => {
+      darkModeQuery.removeListener(handleDarkModeChange);
+    };
+  }, []);
+
+  useEffect(() => {
     if (typeof window !== 'undefined') {
       localStorage.setItem("mode", mode);
     }
   }, [mode]);
+
   return (
     <div className={mode + " theBody"}>
       <Header modeValue={mode} setMode={setMode}/>
