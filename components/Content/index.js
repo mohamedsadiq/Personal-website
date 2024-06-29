b07ea484d818db67f1fb980fcbf07db9f7ade7ab
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2024-06-29 22:32:36 +0200

    update the widget

--- components/Content/index.js
+++ components/Content/index.js
@@ -151,9 +151,13 @@ const svgColorsFun = () => {
       <div className="hover_project latest_activity_project">
         <a href="https://x.com/sadiq_moo/status/1739593994948989280/video/1" target="_blink">
         
-        <Image className="border-rounded " src={"/fffsfs.gif"} alt="" width={100} height={100} style={{ width: "376px", height: "315pxs",borderRadius:"20px", marginBottom:"20px" }} />
-        <h1 className="h_m"> Quick Button </h1>
-        <span>An interactive UI </span>
+        {/* <Image className="border-rounded " src={"/fffsfs.gif"} alt="" width={100} height={100} style={{ width: "376px", height: "315pxs",borderRadius:"20px", marginBottom:"20px" }} /> */}
+        <video width="100%" height="100%" autoPlay loop muted playsInline>
+                    <source src="/dots.mov" type="video/mp4" />
+                    Your browser does not support the video tag.
+          </video>
+        <h1 className="h_m"> Widget</h1>
+        <span>An interactive widget. </span>
         </a>
       </div>
       {/* <div className="hover_project latest_activity_project">
