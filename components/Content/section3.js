8649ca3381c927044d38b6036c68d7e29ff3b45c
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2024-07-26 06:58:57 +0200

    added : motion for home

--- components/Content/section3.js
+++ components/Content/section3.js
@@ -2,38 +2,67 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import productOfTheWeek from "../../public/Optimized SVG.svg";
 import Image from "next/image";
-const Section3 = ({MohamedSadiq}) => {
 
-    
+const Section3 = ({ MohamedSadiq }) => {
+    const hoverEffect = {
+      x: 5, // Adjust the movement distance as needed
+      transition: {
+          duration: 0, // Shorter duration for a snappier effect
+          type: "spring",
+          stiffness: 300, // Higher stiffness for faster response
+          damping: 10 // Adjust damping to control overshoot
+      }
+    };
+
     return (
         <motion.div
-          className="flex flex-col md:flex-row gap-x-6 gap-y-6 md:gap-y-0"
-          initial={MohamedSadiq.initial}
-          animate={MohamedSadiq.animate}
-          transition={{ delay: 0.4 }}
-         >
-          <div className="mainContent flex h-auto w-full md:w-44 flex-none">
-            <h1>Things I’ve built</h1>
-          </div>
-          <div className="mainContent main_projects">
-            <div className="hover_project padding-0">
-              <Link className="mt-0" href="projects/daosspot" passHref>
-                <h1 className="daos_spot_home"> DAOs Spot  <span className="arrowspan"> <span className="relative w-[68px] top-1 inline-block"><Image src={productOfTheWeek} alt="" /></span> ↗</span></h1>
-              </Link>
+            className="flex flex-col md:flex-row gap-x-6 gap-y-6 md:gap-y-0"
+            initial={MohamedSadiq.initial}
+            animate={MohamedSadiq.animate}
+            transition={{ delay: 0.4 }}
+        >
+            <div className="mainContent flex h-auto w-full md:w-44 flex-none">
+                <h1>Things I’ve built</h1>
             </div>
-            <div className="hover_project padding-0">
-              <Link className="mt-2" href="projects/developerdaofm" passHref>
-                <h1 className=""> DeeveloperDAO FM<span className="arrowspan"> <span className="req">Top product PH </span>↗</span></h1>
-              </Link>
+            <div className="mainContent main_projects">
+                <motion.div className="hover_project padding-0" whileHover={hoverEffect}>
+                    <Link className="mt-0" href="projects/daosspot" passHref>
+                        <h1 className="daos_spot_home">
+                            DAOs Spot
+                            <span className="arrowspan">
+                                <span className="relative w-[68px] top-1 inline-block">
+                                    <Image src={productOfTheWeek} alt="" />
+                                </span>
+                                ↗
+                            </span>
+                        </h1>
+                    </Link>
+                </motion.div>
+                <motion.div className="hover_project padding-0" whileHover={hoverEffect}>
+                    <Link className="mt-2" href="projects/developerdaofm" passHref>
+                        <h1 className="">
+                            DeeveloperDAO FM
+                            <span className="arrowspan">
+                                <span className="req">Top product PH </span>
+                                ↗
+                            </span>
+                        </h1>
+                    </Link>
+                </motion.div>
+                <motion.div className="hover_project padding-0" whileHover={hoverEffect}>
+                    <a className="mt-2" href="https://www.behance.net/gallery/134169935/Certified-Web3-Boy-NFTs" passHref>
+                        <h1 className="">
+                            Web3 lover boy NFTs
+                            <span className="arrowspan">
+                                <span className="req">NFTs collection </span>
+                                ↗
+                            </span>
+                        </h1>
+                    </a>
+                </motion.div>
             </div>
-            <div className="hover_project padding-0">
-              <a className="mt-2" href="https://www.behance.net/gallery/134169935/Certified-Web3-Boy-NFTs" passHref>
-                <h1 className=""> Web3 lover boy NFTs<span className="arrowspan"> <span className="req">NFTs collection </span>↗</span></h1>
-              </a>
-            </div>
-          </div>
-        </motion.div >
-    )
+        </motion.div>
+    );
 }
 
-export default Section3
+export default Section3;
--- components/Content/section7.js
+++ components/Content/section7.js
@@ -1,12 +1,10 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-import Img from "../../img/trustwallet.png"
-import Img2 from "../../img/web3cons.png"
-import Img3 from "../../img/Cards design.png"
-import Img4 from "../../img/watchcrypto.png"
-
-
+import Img from "../../img/trustwallet.png";
+import Img2 from "../../img/web3cons.png";
+import Img3 from "../../img/Cards design.png";
+import Img4 from "../../img/watchcrypto.png";
 
 const Section7 = () => {
     const MohamedSadiq = {
@@ -20,31 +18,63 @@ const Section7 = () => {
             top: "0",
             position: "relative"
         }
-    }
+    };
+
+    const hoverState = {
+        stack: {
+            zIndex: 1,
+            scale: 0.97,
+            y: -0,
+            transition: {
+                type: "spring",
+                stiffness: 300
+            }
+        },
+        normal: {
+            zIndex: 0,
+            scale: 1,
+            y: 0,
+            transition: {
+                type: "spring",
+                stiffness: 300
+            }
+        }
+    };
 
     return (
-
         <motion.div
             className="flex flex-col md:flex-row gap-x-6 gap-y-6 md:gap-y-0"
             initial={MohamedSadiq.initial}
             animate={MohamedSadiq.animate}
             transition={{ delay: 0.6 }}
         >
-            <div className="mainContent  flex h-auto w-full md:w-auto flex-none">
+            <div className="mainContent flex h-auto w-full md:w-auto flex-none">
                 <h1 className="">Additional Works</h1>
             </div>
-            <div className="mainContent " id="links_home">
-                <a href="https://dribbble.com/Mohamed-Sadiq" target="_blink">
-                    <div className="flex gap-x-2.5 overflow-x-hidden overflow-y-hidden w-80">
-                        <div className=""> <Image className="rounded-lg" width={100} height={100} alt="" src={Img} /></div>
-                        <div className=""> <Image className="rounded-lg" width={100} height={100} alt="" src={Img2} /></div>
-                        <div className=""> <Image className="rounded-lg" width={115} height={100} alt="" src={Img3} /></div>
-                        <div className=""> <Image className="rounded-lg" width={100} height={100} alt="" src={Img4} /></div>
-                    </div>
+            <div className="mainContent" id="links_home">
+                <a href="https://dribbble.com/Mohamed-Sadiq" target="_blank">
+                    <motion.div
+                        className="flex gap-x-2.5 overflow-x-hidden overflow-y-hidden w-80"
+                        whileHover="stack"
+                        initial="normal"
+                    >
+                        <motion.div variants={hoverState}>
+                            <Image className="rounded-lg" width={100} height={100} alt="" src={Img} />
+                        </motion.div>
+                        <motion.div variants={hoverState}>
+                            <Image className="rounded-lg" width={100} height={100} alt="" src={Img2} />
+                        </motion.div>
+                        <motion.div variants={hoverState}>
+                            <Image className="rounded-lg" width={115} height={100} alt="" src={Img3} />
+                        </motion.div>
+                        <motion.div variants={hoverState}>
+                            <Image className="rounded-lg" width={100} height={100} alt="" src={Img4} />
+                        </motion.div>
+                    </motion.div>
                 </a>
             </div>
         </motion.div>
-    )
-}
+    );
+};
 
-export default Section7
+export default Section7;
