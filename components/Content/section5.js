07289a690e88c09010ff214368e6c6051b9f71f0
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2024-10-08 15:15:43 +0200

    added : circle

--- components/Content/section5.js
+++ components/Content/section5.js
@@ -17,11 +17,11 @@ const Section5 = ({MohamedSadiq}) => {
             <div className="hover_project latest_activity_project">
               <a href="https://x.com/sadiq_moo/status/1811148917783875695" target="_blink">
                 <video width="100%" height="100%" autoPlay loop muted playsInline>
-                  <source src="/July 10 Screen Recording.mp4" type="video/mp4" />
+                  <source src="/ssssad.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
-                <h1 className="h_m text-[#000]"> 3D demo</h1>
-                <span className="text-[#8c8c8c]">Threejs Interactive demo.</span>
+                <h1 className="h_m text-[#000]"> Circle</h1>
+                <span className="text-[#8c8c8c]">A way to display my work.</span>
               </a>
             </div>
           </div>
