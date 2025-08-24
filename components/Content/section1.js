ea82b4d764d6bd683b38bd3c71d1fc394104d182
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2025-08-24 23:05:24 +0200

    added the missing file

--- components/Content/section1.js
+++ components/Content/section1.js
@@ -10,11 +10,10 @@ const Section1 = ({ MohamedSadiq }) => {
     >
       <div id="name" className="p-0 flex h-auto w-full md:w-44 flex-none text-zinc-400">
         Moe Sadiq
-       
       </div>
       <div className="home_text_front">
         <p>
-         A designer and engineer, Molding inspirations into finely crafted realities. Early OSS contributor at{" "}
+         A designer and engineer, Molding inspirations into finely crafted realities. i live to the 0 to 1 journey, Creator of LightUp preveicly, founded Doasspot Early OSS contributor at{" "}
           <a href="https://www.developerdao.com/" target="_blank" rel="noopener noreferrer">
             DeveloperDAO
             <svg className="inline  ml-1" width="14" height="14" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
